ed419b321d6e8caf81bb4848df0d2b86d6f2b942
CokeBeliever <cokebeliever@163.com>
2023-06-05 22:35:47 +0800

    feat[service]: 注册时，对 password 进行 hash 处理

--- service/src/auth/auth.service.ts
+++ service/src/auth/auth.service.ts
@@ -2,15 +2,21 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { SignInDto, SignUpDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
   async signUp(dto: SignUpDto) {
+    const hash = await argon.hash(dto.password);
+
     try {
       const user = await this.prismaService.user.create({
-        data: dto,
+        data: {
+          ...dto,
+          password: hash,
+        },
       });
       return user;
     } catch (error) {
