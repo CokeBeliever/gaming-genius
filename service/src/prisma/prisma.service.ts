ea6f2cf9dc97ec388f4c623b71f83d9a7f8dcdf2
CokeBeliever <cokebeliever@163.com>
2023-06-10 18:24:19 +0800

    feat[service]: 配置和编写 e2e test

--- service/src/prisma/prisma.service.ts
+++ service/src/prisma/prisma.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  clearDb() {
+    return this.$transaction([this.user.deleteMany()]);
+  }
+}
--- service/test/app.e2e-spec.ts
+++ service/test/app.e2e-spec.ts
@@ -1,24 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpDto } from 'src/auth/dto';
+import { ADMIN_EMAIL, ADMIN_PASSWORD } from './utils/constants';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let httpServer: any;
+  let admin: any;
+  let token: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+
+    const prismaService: PrismaService = app.get(PrismaService);
+    await prismaService.clearDb();
+
+    httpServer = app.getHttpServer();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Auth Controller', () => {
+    const signUpDto: SignUpDto = {
+      email: ADMIN_EMAIL,
+      password: ADMIN_PASSWORD,
+    };
+
+    describe('signUp', () => {
+      it('应该注册成功', () => {
+        return request(httpServer)
+          .post('/auth/sign-up')
+          .send(signUpDto)
+          .expect(201)
+          .expect(({ body }) => {
+            expect(body.user).toBeDefined();
+            expect(body.token).toBeDefined();
+          });
+      });
+
+      it('应该注册失败，用户已注册', () => {
+        return request(httpServer)
+          .post('/auth/sign-up')
+          .send(signUpDto)
+          .expect(403);
+      });
+    });
+
+    describe('signIn', () => {
+      it('应该登陆成功', () => {
+        return request(httpServer)
+          .post('/auth/sign-in')
+          .send(signUpDto)
+          .expect(200)
+          .expect(({ body }) => {
+            expect(body.user).toBeDefined();
+            expect(body.token).toBeDefined();
+            admin = body.user;
+            token = body.token;
+          });
+      });
+
+      it('应该登陆失败，凭证不正确', () => {
+        const unRegisteredUser: SignUpDto = {
+          email: 'cokebeliever@163.com',
+          password: 'helloworld',
+        };
+
+        return request(httpServer)
+          .post('/auth/sign-in')
+          .send(unRegisteredUser)
+          .expect(403);
+      });
+    });
+  });
+
+  describe('User Controller', () => {
+    describe('editById', () => {
+      it('应该修改成功', () => {
+        return request(httpServer)
+          .put(`/user/${admin.id}`)
+          .set('authorization', token)
+          .expect(200)
+          .expect(({ body }) => {
+            expect(body).toBeDefined();
+          });
+      });
+    });
   });
 });
